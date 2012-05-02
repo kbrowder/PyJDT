39d2e5c23c5dc359ab327e8a8bdc2a5f9243c50e
KBrowder <kbrowder@gmail.com>
2012-05-01 21:47:24 -0400

    updated for refactoring

--- PyJDT/src/net/kbserve/pyjdt/properties/PyJDTProps.java
+++ PyJDT/src/net/kbserve/pyjdt/properties/PyJDTProps.java
@@ -1,18 +1,13 @@
 package net.kbserve.pyjdt.properties;
 
-import java.io.FileNotFoundException;
-
-import net.kbserve.pyjdt.properties.models_bad.IPersistentProperties;
-import net.kbserve.pyjdt.properties.models_bad.PersistentProperties;
+import net.kbserve.pyjdt.properties.models.RootContainer;
 import net.kbserve.pyjdt.properties.view.PropertyComposite;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.dialogs.PropertyPage;
 
 public class PyJDTProps extends PropertyPage {
@@ -38,7 +33,7 @@ public class PyJDTProps extends PropertyPage {
 
 	protected void performDefaults() {
 		super.performDefaults();
-		PersistentProperties.revert(getElement());
+		RootContainer.revert(getElement());
 		propertyComposite.pack();
 	}
 
@@ -49,16 +44,10 @@ public class PyJDTProps extends PropertyPage {
 
 	@Override
 	public boolean performOk() {
-		// store the value in the owner text field
-		
-		IPersistentProperties pp = PersistentProperties.load(getElement());
-
 		try {
-			pp.save();
+			RootContainer.getRoot(getElement()).save();
 			return true;
-		} catch (CoreException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
