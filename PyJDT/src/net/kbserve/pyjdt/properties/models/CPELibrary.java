aa0f1ff5c7a64ab5be1948a35d464e739822333c
KBrowder <kbrowder@gmail.com>
2012-05-03 22:17:48 -0400

    Fix CPELibraries that are relative to the workspace.

--- PyJDT/src/net/kbserve/pyjdt/properties/models/CPELibrary.java
+++ PyJDT/src/net/kbserve/pyjdt/properties/models/CPELibrary.java
@@ -1,15 +1,24 @@
 package net.kbserve.pyjdt.properties.models;
 
 import org.eclipse.core.resources.IProject;
-
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 
 public class CPELibrary extends CPEAbstractContainer implements ICPEType {
 
 	@Override
 	public String getRealPath(IProject project) {
-		//TODO: what we should do is get the parent path and check if we're relative I thinks.
-		return getPath();//CPE_LIBRARY is always the abspath
+		String realPath = getPath();
+		IPath realIPath = new Path(realPath);
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		IProject origProj = workspaceRoot
+				.getProject(realIPath.segment(0));
+		if (origProj.isAccessible()) {// path is relative to the workspace.
+			IPath workspacePath = workspaceRoot.getLocation().makeAbsolute();
+			return makeStringPath(workspacePath.append(realIPath).makeAbsolute());
+		}
+		return realPath;
 	}
-	
-
 }
