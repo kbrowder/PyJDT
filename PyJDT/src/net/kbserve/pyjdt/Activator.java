a07d0c036da5efe7bae768c3ca1ee6d508f1f1b1
KBrowder <kbrowder@gmail.com>
2012-05-07 20:34:50 -0400

    GPLv3-ize

--- PyJDT/src/net/kbserve/pyjdt/Activator.java
+++ PyJDT/src/net/kbserve/pyjdt/Activator.java
@@ -1,21 +1,48 @@
+/********************************************************************************
+ *  
+ * 
+ *  @author Kevin Browder
+ * 
+ *  Copyright (c) 2012, Kevin Browder All rights reseved
+ * 
+ *  This file is part of PyJDT
+ *  
+ *  PyJDT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *******************************************************************************/
 package net.kbserve.pyjdt;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+// TODO: Auto-generated Javadoc
+
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
  */
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
+	/** The Constant PLUGIN_ID. */
 	public static final String PLUGIN_ID = "net.kbserve.pyjdt"; //$NON-NLS-1$
 
 	// The shared instance
+	/** The plugin. */
 	private static Activator plugin;
 	
 	/**
-	 * The constructor
+	 * The constructor.
 	 */
 	public Activator() {
 		
@@ -40,7 +67,7 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns the shared instance
+	 * Returns the shared instance.
 	 *
 	 * @return the shared instance
 	 */
@@ -50,7 +77,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
+	 * plug-in relative path.
 	 *
 	 * @param path the path
 	 * @return the image descriptor
--- PyJDT/src/net/kbserve/pyjdt/PythonPathContributor.java
+++ PyJDT/src/net/kbserve/pyjdt/PythonPathContributor.java
@@ -1,3 +1,26 @@
+/********************************************************************************
+ *  
+ * 
+ *  @author Kevin Browder
+ * 
+ *  Copyright (c) 2012, Kevin Browder All rights reseved
+ * 
+ *  This file is part of PyJDT
+ *  
+ *  PyJDT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *******************************************************************************/
 package net.kbserve.pyjdt;
 
 import net.kbserve.pyjdt.properties.models.ICPEType;
@@ -9,8 +32,19 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.python.pydev.plugin.nature.IPythonPathContributor;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PythonPathContributor.
+ */
 public class PythonPathContributor implements IPythonPathContributor {
 
+	/**
+	 * Make paths.
+	 *
+	 * @param classpathContainer the classpath container
+	 * @param javaProject the java project
+	 * @return the string buffer
+	 */
 	protected StringBuffer makePaths(ICPEType classpathContainer,
 			IJavaProject javaProject) {
 		IProject project = javaProject.getProject();
@@ -34,6 +68,9 @@ public class PythonPathContributor implements IPythonPathContributor {
 		return sb;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.python.pydev.plugin.nature.IPythonPathContributor#getAdditionalPythonPath(org.eclipse.core.resources.IProject)
+	 */
 	@Override
 	public String getAdditionalPythonPath(IProject project) {
 		IJavaProject javaProject = null;
